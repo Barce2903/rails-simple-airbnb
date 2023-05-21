1df732c215765c907b47770a514c474d796ae8c4
Michael Barcellini <michael.barcellini@gmail.com>
2023-05-21 20:16:34 +0200

    Exercice improved, miss update and delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  flat = Flat.new(
+    name: "Pretty flat in #{Faker::Address.city}",
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(20..500),
+    number_of_guests: rand(1..8)
+  )
+  flat.save!
+end
